cb1593571d16f15538cafb4be4c18a369396e540
Geng Zhao <geng.zhao05@gmail.com>
2020-11-12 12:36:48 -0800

    Minor clean up: address multiple edges to the same node

--- PrefAttachNetwork.py
+++ PrefAttachNetwork.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+import math
 
 # wrappers for generators
 def Beta(a, b): return lambda: np.random.beta(a, b)
@@ -10,7 +11,7 @@ def Unif(a, b): return lambda: np.random.uniform(a, b)
 def Choice(arr, p=None): return lambda: np.random.choice(arr, p=p)
 
 class PAN(object):
-    def __init__(self, m=1, fgen=lambda:1.0, capacity=10000):
+    def __init__(self, m=1, fgen=lambda:1.0, capacity=1000):
         '''Preferential attachment network
         m: degree for each new node
         fgen: fitness distribution
@@ -24,12 +25,15 @@ class PAN(object):
         # self.edges = [[0] * m]                          # edges w/ multiplicity TODO: don't think this is necessary, hard to optimize for performance reason
         self.size = 1                                   # n nodes
         self.tot_edges = m                              # n edges
+        self.weights = self.fs * self.degs
+        self.tot_weights = self.weights[0]
         self._resize(capacity)
 
     def _resize(self, size):
         '''Pad arrays with zeros to avoid repetitive appending'''
         self.degs.resize(size)
         self.fs.resize(size)
+        self.weights.resize(size)
         self.capacity = size
 
     def add_node(self):
@@ -37,12 +41,16 @@ class PAN(object):
         m = self.m
         if size == self.capacity:
             self._resize(self.capacity * 2)
-        weights = np.multiply(self.fs[:size], self.degs[:size])
-        endpoints = np.random.choice(range(size), m, replace=True, p=weights/np.sum(weights))
+        endpoints = np.random.choice(range(size), m, replace=True, p=self.weights[:size]/self.tot_weights)
         # self.edges.append(endpoints)    # new edges
-        self.fs[size] = self.fgen()     # new fitness
-        self.degs[size] = m             # deg for new node
-        self.degs[endpoints] += 1       # update endpoints
+        new_fit = self.fgen()                   # new fitness
+        self.fs[size] = new_fit
+        self.degs[size] = m                     # deg for new node
+        np.add.at(self.degs, endpoints, 1)      # update endpoints TODO: this isn't strictly correct
+        self.weights[size] = m * new_fit
+        endpoints_fit = self.fs[endpoints]
+        np.add.at(self.weights, endpoints, endpoints_fit)
+        self.tot_weights += m * new_fit + math.fsum(endpoints_fit)
         self.size += 1
         self.tot_edges += m
 
