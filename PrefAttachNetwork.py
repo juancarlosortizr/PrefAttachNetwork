f66e33bc9a1831d54c0a797e81af301e0d57a244
juancarlosortizr <jcortiz@stanford.edu>
2020-11-15 18:35:10 -0800

    catch-up times added

--- PrefAttachNetwork.py
+++ PrefAttachNetwork.py
@@ -16,13 +16,17 @@ class PAN(object):
         m: degree for each new node
         fgen: fitness distribution
         capacity(optional): hint for size
+        history: the i-th entry is the list of vertices that the i-th guy connected to when it was created.
+        edges: the i-th entry is the list of vertices that the i-th guy connected to when it was created.
+        Note that the edges guy doesn't update automatically when we create new vertices.
         '''
         self.capacity = capacity
-        self.m = m
+        self.m = m 
         self.degs = np.array([m], dtype='int32')        # degrees
         self.fgen = fgen
         self.fs = np.array([fgen()], dtype='float32')   # fitnesses
-        # self.edges = [[0] * m]                          # edges w/ multiplicity TODO: don't think this is necessary, hard to optimize for performance reason
+        self.history = [[0] * m]                          # edges w/ multiplicity
+        # self.edges = []
         self.size = 1                                   # n nodes
         self.tot_edges = m                              # n edges
         self.weights = self.fs * self.degs
@@ -41,23 +45,58 @@ class PAN(object):
         m = self.m
         if size == self.capacity:
             self._resize(self.capacity * 2)
-        endpoints = np.random.choice(range(size), m, replace=True, p=self.weights[:size]/self.tot_weights)
-        # self.edges.append(endpoints)    # new edges
+        endpoints = np.random.choice(range(size), m, replace=True, p=self.weights[:size]/self.tot_weights) # maybe easier if replace=False?
+        self.history.append(endpoints)    # new edges
         new_fit = self.fgen()                   # new fitness
         self.fs[size] = new_fit
         self.degs[size] = m                     # deg for new node
-        np.add.at(self.degs, endpoints, 1)      # update endpoints TODO: this isn't strictly correct
+        np.add.at(self.degs, endpoints, 1)      # update endpoints TODO: this isn't strictly correct. Juan: Wait why not, multi-edges?
+                                                # I think it's fine if we have multi-edges right?
         self.weights[size] = m * new_fit
         endpoints_fit = self.fs[endpoints]
         np.add.at(self.weights, endpoints, endpoints_fit)
         self.tot_weights += m * new_fit + math.fsum(endpoints_fit)
         self.size += 1
         self.tot_edges += m
+        # self.edges.append([])
+
+    # def update_edges(self):
+    #     """ We update self.edges to its correct values, using self.history."""
+    #     size = self.size
+    #     for i in range(size):
+    #         for j in self.history[i]:
+    #             self.edges[i].append(j)
+    #             if i!=j:
+    #                 self.edges[j].append(i)
 
     def grow_to_size(self, size):
         if size > self.capacity: self._resize(size)
         while (self.size < size):
             self.add_node()
+        # self.update_edges()
+
+    def catchup_time(self, i,j):
+        """ returns the catch-up time for vertex i to catchup to vertex j, i.e. minimal time such that deg(i) >= deg(j)
+        Assumptions: i>j, fitness(i) > fitness(j), and i<size. If these aren't true, we return None.
+        Also assume that self.edges is updated. If it doesn't catchup on time, return False"""
+        size = self.size
+        if i<=j or i>=size: 
+            return
+        if self.fs[i] <= self.fs[j]: 
+            return
+        curr_time = j
+        deg_j = m
+        deg_i = 0
+
+        while curr_time < size and deg_i < deg_j:
+            curr_time += 1
+            new_neighbours = np.array(self.history[curr_time]) # this bit of code is easier with replace=False 
+            deg_j += np.count_nonzero(new_neighbours == j)
+            deg_i += np.count_nonzero(new_neighbours == i)
+
+        if deg_i >= deg_j: return curr_time 
+        return False
+
 
 if __name__ == '__main__':
     pan = PAN()
--- experiment.py
+++ experiment.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PrefAttachNetwork import *
 
+FINAL_SIZE = 1000
+NUM_SAMPLES_CATCHUP = 1
+
 plt.rc('xtick', labelsize=13)
 plt.rc('ytick', labelsize=13)
 plt.rc('legend', fontsize=13)
@@ -13,12 +16,12 @@ plt.rc('figure', figsize=[8,6])
 
 pan_plain = PAN(m=5)
 pan_beta = PAN(m=5, fgen=Beta(1,10))
-pan_plain.grow_to_size(100000)
-pan_beta.grow_to_size(100000)
+pan_plain.grow_to_size(FINAL_SIZE)
+pan_beta.grow_to_size(FINAL_SIZE)
 
 # Degree distribution for beta fitnesses vs flat fitness
-plt.hist(np.log(pan_plain.degs), bins=50, alpha=0.8, normed=True)
-plt.hist(np.log(pan_beta.degs), bins=50, alpha=0.8, normed=True)
+plt.hist(np.log(pan_plain.degs), bins=50, alpha=0.8)#, normed=True)
+plt.hist(np.log(pan_beta.degs), bins=50, alpha=0.8)#, normed=True)
 plt.yscale('log')
 plt.xlabel('log degree')
 plt.legend(['Flat fitness', 'Beta(1,10) fitness'])
@@ -28,9 +31,54 @@ plt.show()
 fitnesses = [1,2,3]
 pan_discrete = PAN(fgen=Choice([1,2,3], p=[0.5, 0.3, 0.2]))
 pan_discrete.grow_to_size(100000)
-plt.hist([np.log(pan_discrete.degs[pan_discrete.fs==f]) for f in fitnesses], bins=20, normed=True, label=['f='+str(f) for f in fitnesses])
+plt.hist([np.log(pan_discrete.degs[pan_discrete.fs==f]) for f in fitnesses], bins=20, label=['f='+str(f) for f in fitnesses])#, normed=True)
 plt.yscale('log')
 plt.xlabel('log degree')
 plt.legend()
 plt.show()
 
+# Some catch-up times for the plain model
+# Vertices i and j with i>j, fitness[i] > fitness[j], alpha = i/j, beta = fitness[i]/fitness[j], catch-up time t. 
+# We expect t/i to be roughly equal to alpha^{1/(beta-1)}. (our "prediction")
+# Independetly of the fitness distribution!
+pan_plain_1 = PAN(m=1)
+pan_beta_1 = PAN(m=1, fgen=Beta(1,10))
+pan_plan_1.grow_to_size(FINAL_SIZE)
+pan_beta_1.grow_to_size(FINAL_SIZE)
+fractions_time_plain = []
+predictions_plain = []
+fractions_time_beta = []
+predictions_beta = []
+
+for _ in range(NUM_SAMPLES_CATCHUP):
+	size = pan_plain_1.size()
+	while True:
+		[i,j] = np.random.choice(size, size=2, replace=False)
+		catchup_time = pan_plain_1.catchup_time(i,j)
+		if catchup_time != None and catchup_time != False:
+			break
+	predictions_plain.append(pow(i/j, pan_plain_1.fs[i]/pan_plain_1.fs[j]))
+	fractions_time_plain.append(catchup_time/i)
+
+	size = pan_beta_1.size()
+	while True:
+		[i,j] = np.random.choice(size, size=2, replace=False)
+		catchup_time = pan_beta_1.catchup_time(i,j)
+		if catchup_time != None and catchup_time != False:
+			break
+	predictions_beta.append(pow(i/j, pan_plain_1.fs[i]/pan_plain_1.fs[j]))
+	fractions_time_beta.append(catchup_time/i)
+
+plt.scatter(fractions_time_plain, predictions_plain)
+plt.xlabel('Fractions t/t_A')
+plt.ylabel('Predicted catch-up time')
+plt.title('Catch-up times, plain distribution')
+plt.legend()
+plt.show()
+
+plt.scatter(fractions_time_beta, predictions_beta)
+plt.xlabel('Fractions t/t_A')
+plt.ylabel('Predicted catch-up time')
+plt.title('Catch-up times, Beta distribution')
+plt.legend()
+plt.show()
